d613fe2b1d60da5019aa54e143ef4e9078570f0b
Maks Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-04-13 13:30:28 +0200

    Add sorted tasks on client. Add css for ListItem

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
 import ListHeader from './components/ListHeader';
+import ListItem from './components/ListItem'
 import { useEffect, useState } from 'react'
 
 const App = () => {
   const userEmail = 'maks@test.com';
-  const [tasks, setTasks] = useState(null);
+  const [ tasks, setTasks ] = useState(null);
 
-const getData = async () => {
-  try {
-    const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
-    const json = await response.json();
-    setTasks(json);
+  const getData = async () => {
+  
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+      setTasks(json);
     } catch (err) {
-    console.error(err);
+      console.error(err);
+    }  
   }
-}
 
 useEffect(() => getData, []);
 
-console.log(tasks)
+console.log(tasks);
+
+// sort by date
+const sortedTasks = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date));
 
   return (
     <div className="app">
       <ListHeader listName={'name of a todo'} />
+      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} />)}
     </div>
   );
 }
