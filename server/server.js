873d6797f99c46ae709c1386f6421fd9cb30f5b6
Maks Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-04-13 12:20:11 +0200

    Delete console.log()

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 const express = require('express');
 const cors = require('cors');
@@ -10,9 +11,8 @@ app.use(cors());
 // get all todos
 app.get('/todos/:userEmail', async(req, res) => {
   const { userEmail } = req.params;
-  console.log(userEmail);
   try {
-    const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
+    const todos = await pool.query('SELECT * FROM todos WHERE user_email= $1', [userEmail]);
     res.json(todos.rows);
   } catch (err) {
     console.error(err);
