e7a3c7cbae7cb3dd7b49c2885c4d6f4ca155489f
Maks Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-04-13 11:28:43 +0200

    Add basic client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import ListHeader from './components/ListHeader';
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const userEmail = 'maks@test.com';
+  const [tasks, setTasks] = useState(null);
+
+const getData = async () => {
+  try {
+    const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+    const json = await response.json();
+    setTasks(json);
+    } catch (err) {
+    console.error(err);
+  }
+}
+
+useEffect(() => getData, []);
+
+console.log(tasks)
+
   return (
     <div className="app">
-      <ListHeader listName={'Holiday Tick List'} />
+      <ListHeader listName={'name of a todo'} />
     </div>
   );
 }
