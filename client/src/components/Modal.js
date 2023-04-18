0bcd14f87ed96791bd5c6da2628a9d143c570252
Maks Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-04-18 16:08:02 +0200

    Replace hardcoded localhost by client/.env

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -13,7 +13,7 @@ const Modal = ({ mode, setShowModal, getData, task }) => {
   const postData = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:8000/todos', {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos`, {
         method: "POST",
         headers: {'Content-Type': 'application/json' },
         body: JSON.stringify(data)
@@ -31,7 +31,7 @@ const Modal = ({ mode, setShowModal, getData, task }) => {
   const editData = async(e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://localhost:8000/todos/${task.id}`, {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${task.id}`, {
         method: "PUT",
         headers: { 'Content-Type' : 'application/json' },
         body: JSON.stringify(data)
