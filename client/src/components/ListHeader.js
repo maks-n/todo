3c624f006eb4a31371340e7d27c36ead4eae2fd4
Maks Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-04-18 18:35:23 +0200

    Add logout feature

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,11 +1,16 @@
-import { useState } from 'react';
 import Modal from './Modal';
+import { useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 const ListHeader = ({ listName, getData }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
   const [showModal, setShowModal] = useState(false);
 
   const signOut = () => {
-    console.log('signout')
+    console.log('signout');
+    removeCookie('Email');
+    removeCookie('AuthToken');
+    window.location.reload();
   }
   
   return (
@@ -13,7 +18,7 @@ const ListHeader = ({ listName, getData }) => {
       <h1>{listName}</h1>
       <div className="button-container">
         <button className="create" onClick={() => setShowModal(true)}>ADD NEW</button>
-        <button className="signout" onCLick={signOut}>SIGN OUT</button>
+        <button className="signout" onClick={signOut}>SIGN OUT</button>
       </div>
       {showModal && <Modal mode={'create'} setShowModal={setShowModal} getData={getData}/>}
     </div>
