f488f40bbfecf5d2891619ee194234bdea0999f2
Maks Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-04-18 18:12:54 +0200

    Add cookies, jwt, password encryption and signup feature using all of these

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import ListHeader from './components/ListHeader';
 import ListItem from './components/ListItem'
 import Auth from './components/Auth'
 import { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie';
 
 const App = () => {
-  const userEmail = 'maks@test.com';
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const authToken = cookies.AuthToken;
+  const userEmail = cookies.Email;
   const [ tasks, setTasks ] = useState(null);
-
-  const authToken = false;
 
   const getData = async () => {
     try {
