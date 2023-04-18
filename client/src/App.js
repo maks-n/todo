a69d9bb58ad563ac53b8045bc25c9adf1278aefe
Maks Nazarenko <71183975+maks-n@users.noreply.github.com>
2023-04-18 16:46:12 +0200

    Add authentication feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import ListHeader from './components/ListHeader';
 import ListItem from './components/ListItem'
+import Auth from './components/Auth'
 import { useEffect, useState } from 'react'
 
 const App = () => {
   const userEmail = 'maks@test.com';
   const [ tasks, setTasks ] = useState(null);
 
+  const authToken = false;
+
   const getData = async () => {
-  
     try {
       const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`);
       const json = await response.json();
@@ -17,7 +19,11 @@ const App = () => {
     }  
   }
 
-useEffect(() => getData, []);
+useEffect(() => {
+  if (authToken) {
+    getData();
+  }
+}, [])
 
 console.log(tasks);
 
@@ -26,8 +32,13 @@ const sortedTasks = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date));
 
   return (
     <div className="app">
-      <ListHeader listName={'name of a todo'} getData={getData}/>
-      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData}/>)}
+      {!authToken && <Auth/>}
+      {authToken &&
+        <>
+        <ListHeader listName={'name of a todo'} getData={getData}/>
+        {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData}/>)}
+        </>
+      }
     </div>
   );
 }
